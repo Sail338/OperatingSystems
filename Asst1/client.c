5ce3f869e4087a98fa643dfea2ad15e342585002
Sail338 <srihari.shankar1@gmail.com>
2018-02-06 23:46:32 -0500

    pushed updated client

--- Asst1/client.c
+++ Asst1/client.c
@@ -1,16 +1,26 @@
 #include "my_pthread_t.h"
 #include <unistd.h>
-int func(){
+int func(int x){
 	printf("new thread created\n");
 	printf("HELLO WORLD\n");
 	printf("KEK\n");
-	setcontext(&(scheduler->current->thread));
+	printf("x is %d\n",x);
+	swapcontext(&(scheduler->tq[0]->front->thread),&(scheduler->current->thread));
 	return  0;
 
 }
 
 int main(){
 		my_pthread_t t;
-		my_pthread_create(&t,NULL,(void*)func,NULL);
+		typedef struct args {
+			int arg1;
+			int arg2;
+
+		}args;
+		args arg;
+	   arg.arg1 = 5;
+   	    arg . arg2 = 6;	   
+		my_pthread_create(&t,NULL,(void*)func,(void *)5);
+		printf("Back in main bois");
 }
 
