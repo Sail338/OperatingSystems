56143c48d4f33bb862f986f03d863a5673d17916
Sail338 <srihari.shankar1@gmail.com>
2018-02-08 23:25:20 -0500

    pushed client

--- Asst1/client.c
+++ Asst1/client.c
@@ -12,32 +12,22 @@ int func(int x){
 	return  0;
 
 }
-int func1(int x){
+int func1(){
     printf("Currently in second thread\n");
-    int i = 0;
-    while(i < 2000000000){printf("IM THREAD2\n");}
-    printf("x is %d\n",x);
-    my_pthread_exit(NULL);
+	int *x = malloc(sizeof(int));
+	*x = 9;
+	printf("prepare to exit on the right \n");
+	my_pthread_exit(x);
     return 0;
 }
 
 
 int main(){
-		my_pthread_t t;
         my_pthread_t z;
-		typedef struct args {
-			int arg1;
-			int arg2;
-
-		}args;
-		args arg;
-	   arg.arg1 = 5;
-   	    arg . arg2 = 6;
-        my_pthread_create(&z,NULL,(void*)func1,(void*)9);
-		my_pthread_create(&t,NULL,(void*)func,(void *)5);
-        printf("HAAHAHAHAHAHAHA1\n");
-        int i = 0;
-        while(i<2000000000){printf("IM MAIN!\n");}
+        my_pthread_create(&z,NULL,(void*)func1,NULL);
+		int *holder;
+		my_pthread_join(z,(void **)&holder);
+		printf("%d\n",*holder);
         printf("HAAHAHAHAHAHAHA2\n");
 	//	printf("Back in main bois");
 }
