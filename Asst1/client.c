153a66c38ade990fa95e5b76c27d754b2e608093
Sail338 <srihari.shankar1@gmail.com>
2018-02-12 00:15:13 -0500

    push client

--- Asst1/client.c
+++ Asst1/client.c
@@ -1,17 +1,12 @@
 #include "my_pthread_t.h"
 #include <unistd.h>
 my_pthread_mutex_t mutex;
+int indicator =0;
 
-
-int func(int indicator){
-	int lockable = my_pthread_mutex_lock(&mutex);
-	if (lockable == 0)
-			printf("successful initial lock\n");
-	else
-	{
-		indicator = 10;
-		printf("failed initial lock\n");
-	}
+int func(){
+	 my_pthread_mutex_lock(&mutex);
+	 indicator ++;
+	 printf("%d",indicator);
 	//swapcontext(&(scheduler->tq[0]->front->thread),&(scheduler->current->thread));
     
 	my_pthread_exit(NULL);
@@ -22,13 +17,12 @@ int func(int indicator){
 int main(){
 		
 		my_pthread_t t;
-		int indicator = 0;
+		my_pthread_t t1;
 		my_pthread_mutex_init(&mutex, NULL);
 		my_pthread_create(&t,NULL,(void*)func,(void *)5);
-		int locked = my_pthread_mutex_lock(&mutex);
-		if (locked == -1)
-		{
-			printf("DEADLOCK WOOOO!\n");
+		my_pthread_create(&t1,NULL,(void*)func,NULL);
+		while(true){
+
 		}
 		return 0;
 }
