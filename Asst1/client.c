020f051da3d907bb1002770bf0e69698671a11db
Sam Azouzi <sazouzi21@gmail.com>
2018-02-13 01:12:11 -0500

    another bug to relax

--- Asst1/client.c
+++ Asst1/client.c
@@ -6,14 +6,24 @@ int indicator =0;
 int func(int val){
 	 my_pthread_mutex_lock(&mutex);
 	 indicator ++;
-	 printf("%d\n",indicator);
+	 printf("%d\t%d\n",indicator,val);
+     int i = 0;
+     while(i < 200000000){
+        i+=1;
+     }
 	 my_pthread_mutex_unlock(&mutex);
+     printf("Finished Unlocked\n");
 	//swapcontext(&(scheduler->tq[0]->front->thread),&(scheduler->current->thread));    
 	//my_pthread_exit(NULL);
 	return  0;
 
 }
-
+int func1(int val){
+    my_pthread_mutex_lock(&mutex);
+    indicator+=1;
+    printf("%d\t%d\n",indicator,val);
+    return 0;
+}
 
 int main(){
 		
@@ -21,7 +31,7 @@ int main(){
 		my_pthread_t t1;
 		my_pthread_mutex_init(&mutex, NULL);
 		my_pthread_create(&t,NULL,(void*)func,(void *)1);
-		my_pthread_create(&t1,NULL,(void*)func,(void*)2);
+		my_pthread_create(&t1,NULL,(void*)func1,(void*)2);
         int i=0;
 		while (true){
 			}
