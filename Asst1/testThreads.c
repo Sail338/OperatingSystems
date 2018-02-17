a221d9193116aa6d3eee3ddeffa6e186e322a5bc
Sam Azouzi <sazouzi21@gmail.com>
2018-02-17 18:37:25 -0500

    testing write files is totes done

--- Asst1/testThreads.c
+++ Asst1/testThreads.c
@@ -11,15 +11,17 @@ int testFunc(void *  val){
     for(i = 0; i < 200000; i++){
         fprintf(fp,"IN THREAD %d: Current Val: %d\n",*((int*)val),i);
     }
-    //if(*((int*)val) >= 100){
-    //    int v = *((int*)val);
-    //    int z;
-    //    for(z = v-100; z < v; z++){
-    //        my_pthread_join(list[z],NULL);
-    //    }
-    //}
-    fprintf(fp,"I WAITED ON THE LAST 100 THREADS!\n");
+    if(*((int*)val) > 100){
+        int v = *((int*)val);
+        int z;
+        for(z = v-100; z < v; z++){
+            my_pthread_join(list[z],NULL);
+            fprintf(fp,"Finished Joing Thread: %d\n",z);
+        }
+        fprintf(fp,"I WAITED ON THE LAST 100 THREADS!\n");
+    }
     fclose(fp);
+    printf("FINISHED WITH THREAD: %d\n",*(int*)val);
     return 0;
 }
 
@@ -30,7 +32,7 @@ int testFuncSync(void *  val){
     FILE * fp = fopen(fileName,"a");
     int i;
     for(i = 0; i < 200000; i++){
-        fprintf(fp,"IN SYNC CALL %d: Current Val: %d\n",*((int*)val),i);
+        fprintf(fp, "IN SYNC %d: Current Val: %d\n",*((int*)val),i);
     }
     fclose(fp);
     return 0;
@@ -42,22 +44,17 @@ my_pthread_t * testThreads(int num){
     int * z = malloc(sizeof(int)*num*5);
     int nameOfFile = 0;
     for(i = 0; i < num; i++){
-        int done = 0;
-        while(done <= 5){
-            z[i] = nameOfFile;
-            my_pthread_create(&list[i], NULL,(void*)testFunc,&z[i]);
-            done+=1;
-            nameOfFile+=1;
-        }
+        z[i] = nameOfFile;
+        my_pthread_create(&list[i], NULL,(void*)testFunc,&z[i]);
+        nameOfFile+=1;
     }
-    free(z);
     return list;
 
 }
 
 void testSync(int num){
     int i;
-    for(i = num*5; i < num*2*5; i++){
+    for(i = num; i < num*2; i++){
         int * x = malloc(sizeof(int));
         *x = i;
         testFuncSync((void*)x);
@@ -67,8 +64,9 @@ void testSync(int num){
 
 int main(){
         clock_t begin = clock();
-        int size = 20;
+        int size = 110;
         list = testThreads(size);
+        printf("All Threads Created!");
         int i = 0;
         int * x;
         while( i < size){
@@ -78,11 +76,11 @@ int main(){
         clock_t end = clock();
         double totTime = (double)(end-begin)/CLOCKS_PER_SEC;
         printf("THREAD FIN\nTIME: %f\n\n\n",totTime);
-        begin = clock();
-        testSync(size);
-        end = clock();
-        totTime = (double)(end-begin)/CLOCKS_PER_SEC;
-        printf("SYNC FIN\nTIME: %f\n",totTime);
+        //begin = clock();
+        //testSync(size);
+        //end = clock();
+        //totTime = (double)(end-begin)/CLOCKS_PER_SEC;
+        //printf("SYNC FIN\nTIME: %f\n",totTime);
         return 0;
 }
 
