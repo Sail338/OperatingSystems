d2fb0ab53821523a2df9d920fb8cef3e2802c241
Sail338 <srihari.shankar1@gmail.com>
2018-03-12 19:23:58 -0400

    push driver

--- Asst2/driver.c
+++ Asst2/driver.c
@@ -1,11 +1,14 @@
 #include "my_pthread_t.h"
 
 int main(){
-	int *x = (int *)mymalloc(4);
-	*x = 1;
-	int * y = (int *) mymalloc(4);
-	*y = 2;
-	
-	printf("x addr: %p, y addr: %p\n", x, y);
+	int *x = (int *)mymalloc(1360);
+	int *y = (int*)mymalloc(1360);	
+	int *z =(int*)mymalloc(1360);
+	my_free(y);
+	my_free(z);
+	int *test = mymalloc(2720);
+	*test =10;
 
+	printf("x add %p y addr %p z addr %p test adress %p \n",x,y,z,test);
+	printf("TEST VALUE is %d",*test);
 }
