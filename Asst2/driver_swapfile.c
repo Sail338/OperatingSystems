4903e80c673c0a40c84ee7f9f40c87e65151784d
Sam Azouzi <sazouzi21@gmail.com>
2018-03-21 17:10:28 -0400

    added driver for swapfile testing

---
+++ Asst2/driver_swapfile.c
@@ -0,0 +1,18 @@
+#include "my_pthread_t.h"
+
+
+
+void func()
+{
+    char * y = mymalloc(4000);
+    page_table_string(0,4);
+    return;
+}
+
+int main()
+{
+    char * x = mymalloc(7884800-8);
+    pthread_t t;
+    //pthread_create(&t,NULL,(void*)func,NULL);
+    return 0;
+}
