07285da5e80d58ac6cedf08a625db1cf36883ffb
Sail338 <srihari.shankar1@gmail.com>
2018-04-03 21:37:09 -0400

    adds util

---
+++ Asst3/src/util.h
@@ -0,0 +1,46 @@
+nclude <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <stdbool.h>
+int IS_FILE_TABLE_INIT;
+typedef struct Inode
+{
+			//we only care aboyt the positon for the first inode in the chain
+				int file_position;
+				int fd;
+				//permissions the file was created with
+				int permissions;
+						//permssions the file currently using
+				int  file_mode;
+			//is this a file or folder
+				char  file_type;
+	
+				//path
+				char *path;
+				//timestamp
+				time_t timestamp;
+				//above is what we only care about in the frist thing in the chain
+				//space left in INODE
+				int spaceleft;
+				//pointer to next inode in the chain
+			    struct Inode *  next;
+				struct Inode *prev;
+				bool is_init;
+				//int linkcount;
+			
+}Inode;
+			
+			
+			
+			
+/*
+	*table that maps Inodes and stutff
+*/
+typedef struct FileTable
+{
+	Inode ** files;
+	int num_free_inodes;
+			
+			
+}FileTable;G
